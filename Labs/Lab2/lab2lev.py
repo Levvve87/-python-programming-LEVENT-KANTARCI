0467797d4eefb73bfec27a21e3cfe018be820d30
Levent Kantarci <levent.kantarci@iths.se>
2024-10-10 22:06:55 +0200

    Tredje commit

--- Labs/Lab2/lab2lev.py
+++ Labs/Lab2/lab2lev.py
@@ -63,4 +63,43 @@ plt.legend()
 
 plt.grid()
 
-plt.show()     
+plt.show()
+
+def euclidean_distance(x2, x1, y2, y1):
+    
+    return mt.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+
+testpokemon = "testpoints.txt"
+
+with open(testpokemon, "r") as testpokemons:
+  
+    for testdata in testpokemons:
+        
+        if testdata.startswith("T"):
+            continue
+        
+        cleaned_testdata = testdata.strip().replace("(", "").replace(")", "").replace(",", "")
+        
+        more_cleaned_testdata = cleaned_testdata.split()
+       
+        test_width = float(more_cleaned_testdata[1])
+        test_height = float(more_cleaned_testdata[2])
+        
+        nearest_distance = float("inf")
+        nearest_label = None 
+       
+        for i in range(len(widths)):
+       
+            distance = euclidean_distance(test_width, widths[i], test_height, heights[i])
+           
+            if distance < nearest_distance:
+                nearest_distance = distance
+                nearest_label = labels[i]
+        
+        if nearest_label == 0:
+            print(f"Sample with (width, height): ({test_width}, {test_height}) classified as Pokemon - Pichu")
+        else:
+            print(f"Sample with (width, height): ({test_width}, {test_height}) classified as Pokemon - Pikachu")
+
+
